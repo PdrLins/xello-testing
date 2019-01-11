e6201c8aceecbeb9c6fb0941f706c2af08a240d9
Pedro Lins <paugustolins@gmail.com>
2019-01-11 16:51:06 -0500

    css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowroomComponent } from '../app/showroom/showroom.component';
+import { WellcomeComponent } from '../app/wellcome/wellcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'showroom', component: ShowroomComponent },
+  { path: 'wellcome', component: WellcomeComponent },
+  { path: '', redirectTo:'/wellcome', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowroomComponent } from './showroom/showroom.component';
+import { WellcomeComponent } from './wellcome/wellcome.component';
+
+// import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap/tooltip/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowroomComponent,
+    WellcomeComponent
+    // NgbTooltip
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
